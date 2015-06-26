bb6e57e85a56362352793a69acbb37c9c463e155
team 502 <tearstopromise@gmail.com>
2015-06-26 14:53:20 +0800

    asd
    dfg

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// Phonegap Barcode scanner
+// Author: Umer Pasha
+// Date: 8/26/14
+
+var resultDiv;
+
+document.addEventListener("deviceready", init, false);
+function init() {
+	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
+	resultDiv = document.querySelector("#results");
+}
+
+function startScan() {
+
+	cordova.plugins.barcodeScanner.scan(
+		function (result) {
+			var s = "Result: " + result.text + "<br/>" +
+			"Format: " + result.format + "<br/>" +
+			"Cancelled: " + result.cancelled;
+			resultDiv.innerHTML = s;
+		}, 
+		function (error) {
+			alert("Scanning failed: " + error);
+		}
+	);
+
+}
